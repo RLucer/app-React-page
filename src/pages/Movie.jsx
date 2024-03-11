9411cade4ffad60146ba3e6f3e9457a06724b1cd
RLucer <lucero.ricardo@gmail.com>
2024-03-11 08:34:53 -0300

    added data.json

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,22 +1,24 @@
-import React from 'react'
-import data from '../data.json';
-import {Button, Card } from 'react-bootstrap'
+import React from "react";
+import data from "../data.json";
+import { Button, Card, Col, Row } from "react-bootstrap";
 export const Movie = () => {
   return (
- <div>
-    {
-      data.movies.map(movie =>(
-        <Card key={movie.id} style={{with:'18rem'}}>
-          <Card.Img variant ='top' src={movie.image} />
-          <Card.Body>
-            <Card.Title>{movie.title}</Card.Title>
-            <Card.Text>{movie.years}</Card.Text>
-            <Button> Search </Button>
-          </Card.Body>
-
-        </Card>
-      ) )
-    }
- </div>
-  )
-}
+    <div>
+      <Row>
+        <Col sm={6}>
+          {data.movies.map((movie) => (
+            <Card key={movie.id} style={{ with: "18rem" }}>
+              <Card.Img variant="top" src={movie.image} />
+              <Card.Body>
+                <Card.Title>{movie.title}</Card.Title>
+                <Card.Text>{movie.years}</Card.Text>
+                <Button> Search </Button>
+              </Card.Body>
+            </Card>
+          ))}
+        </Col>
+        <Col sm={6}></Col>
+      </Row>
+    </div>
+  );
+};
